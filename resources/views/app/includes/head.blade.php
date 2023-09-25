ba3cf1734d42528f63f4c0ebe6a6e43ee35c576a
haseeb3294 <98734313+haseeb3294@users.noreply.github.com>
2023-09-25 13:37:36 +0500

    first commit

---
+++ resources/views/app/includes/head.blade.php
@@ -0,0 +1,7 @@
+<!-- BEGIN: Theme CSS-->
+<link rel="stylesheet" href="{{asset('assets/app/css/rt-plugins.css')}}">
+<link rel="stylesheet" href="{{asset('assets/app/css/app.css')}}">
+<!-- End : Theme CSS -->
+
+<!-- Start : theme-store js -->
+<script src="{{asset('assets/app/js/store.js')}}" sync=""></script>
---
+++ resources/views/app/includes/scripts.blade.php
@@ -0,0 +1,4 @@
+
+<script src="{{asset('assets/app/js/jquery-3.6.0.min.js')}}"></script>
+<script src="{{asset('assets/app/js/rt-plugins.js')}}"></script>
+<script src="{{asset('assets/app/js/app.js')}}"></script>
